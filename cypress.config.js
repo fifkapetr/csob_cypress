4e8ff9a5de3aa4425b373ced591f7181734be82b
Petr Fifka <petrfifk@gmail.com>
2024-01-18 09:24:29 +0100

    add env and baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    pmtool_url: "http://tredgate.com/pmtool/index.php?",
+    eshop_url: "http://tredgate.com/eshop",
+    webtrain_url: "http://tredgate.com/webtrain",
+    automation_test_store_url: "https://automationteststore.com/",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://tredgate.com/pmtool",
   },
   watchForFileChanges: false,
   defaultCommandTimeout: 10000,
